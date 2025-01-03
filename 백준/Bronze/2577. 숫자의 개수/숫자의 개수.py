573d2500df17aba24bdb697afa5cab20daf8b001
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-01-03 18:47:12 +0900

    Update 숫자의 개수.py

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,13 +1,4 @@
-num = 1
-
-for _ in range(3):
-    num = num * int(input())
+num = [int(input()) for _ in range(3)]
     
-num = str(num)
-numbers = [i for i in range(10)]
-
 for i in range(10):
-    numbers[i] = num.count(str(numbers[i]))
-    
-for j in numbers:
-    print(j)
+    print(str(num[0] * num[1] * num[2]).count(str(i)))
