dc640f62c1a08fe67212e0126ef00e05079edb2b
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-13 02:54:55 +0900

    [Silver III] Title: 피보나치 함수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,16 +1,17 @@
-def fibonacci(n):
-    a, b = 1, 0
-    if n == 0:
-        return a, b
-    elif n == 1:
-        return b, a
-    else:
-        temp = fibonacci(n-2)
-        a = sum(temp)
-        b = a + temp[1]
-        return a, b
-
 T = int(input())
+num = [int(input()) for _ in range(T)]
+m = max(num)
+dp = [[0, 0] for _ in range(m+1)]
+dp[0][0] = 1
 
-for _ in range(T):
-    print(*fibonacci(int(input())))
+for i in range(m+1):
+    if i == 0:
+        pass
+    elif i == 1:
+        dp[1][1] = 1
+    else:
+        dp[i][0] = dp[i-2][0] + dp[i-2][1]
+        dp[i][1] = dp[i-1][0] + dp[i-1][1]
+        
+for j in num:
+    print(*dp[j])
