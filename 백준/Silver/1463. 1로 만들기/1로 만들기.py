c9b02ab697f3d72e3eeb99474228aa28b3157efe
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-14 03:32:03 +0900

    [Silver III] Title: 1로 만들기, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,9 @@
 N = int(input())
-dp = [0] * (N + 1)
 
-for i in range(2, N+1):
-    dp[i] = dp[i-1] + 1
-    if i % 6 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1, dp[i//3] + 1)
-    elif i % 2 == 0:
-        dp[i] = min(dp[i], dp[i//2] + 1)
-    elif i % 3 == 0:
-        dp[i] = min(dp[i], dp[i//3] + 1)
+def makeone(n):
+    if n < 3:
+        return n-1
+    else:
+        return 1 + min(makeone(n//2) + n%2, makeone(n//3) + n%3)
         
-print(dp[N])
+print(makeone(N))
