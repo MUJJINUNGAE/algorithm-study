49e881fc0067f417ed7e0133e32704c799035c4c
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-15 17:06:53 +0900

    [Bronze II] Title: Hashing, Time: 112 ms, Memory: 36944 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,18 @@
+import string
+
+alphabet_lower = string.ascii_lowercase
+match_alpabet = dict()
+
+for i in range(len(alphabet_lower)):
+    match_alpabet[alphabet_lower[i]] = i+1
+    
+L = int(input())
+r = 31
+M = 1234567891
+a = input()
+
+result = 0
+for i in range(len(a)):
+    result += match_alpabet[a[i]] * (r**i)
+    
+print(result)
