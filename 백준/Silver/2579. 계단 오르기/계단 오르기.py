7637c0f6704b1ed8b70c35a54ed4c59fa7b46490
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-18 16:53:45 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -2,12 +2,10 @@ N = int(input())
 step = [0] + [int(input()) for _ in range(N)]
 dp = [0] * (N+1)
 
-for i in range(1, N+1):
-    if N == 1:
-        dp[1] = step[1]
-    elif N == 2:
-        dp[2] = step[1] + step[2]
-    else:
+if N < 3:
+    dp[N] = sum(step)
+else:
+    for i in range(1, N+1):
         dp[i] = dp[i-2] + step[i]
         dp[i] = max(dp[i], dp[i-3] + step[i-1] + step[i])
 
