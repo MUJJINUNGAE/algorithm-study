18e50b6df58d9d7d18a4fdc65b1f41aff44793a7
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-18 19:57:00 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -0,0 +1,15 @@
+T = int(input())
+
+PS = [input() for _ in range(T)]
+
+for s in PS: 
+  while True:
+      if '()' in s:
+          s = s.replace('()', '')
+      else:
+          break
+      
+  if s == '':
+      print('YES')
+  else:
+      print('NO')
