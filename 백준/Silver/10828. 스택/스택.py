fa1d8d26296c1cd61410e85c6bd7b1abeca01eb5
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-19 22:54:43 +0900

    [Silver IV] Title: 스택, Time: 64 ms, Memory: 34900 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,20 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+stack = deque()
+
+for _ in range(N):
+    command = input().split()
+    if command[0] == 'push':
+        stack.append(command[1])
+    elif command[0] == 'pop':
+        print(stack.pop()) if len(stack) != 0 else print(-1)
+    elif command[0] == 'size':
+        print(len(stack))
+    elif command[0] == 'empty':
+        print(1) if len(stack) == 0 else print(0)
+    elif command[0] == 'top':
+        print(stack[-1]) if len(stack) != 0 else print(-1)
