65d75b18bb49c9e2a07e2daab1c39463d758d56a
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-20 01:48:27 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -1,16 +1,10 @@
-from collections import deque
-
 N, K = map(int, input().split())
-pm = deque([i for i in range(1, N+1)])
+pm = list(range(1, N+1))
 josephus = []
-cnt = 0
+idx = 0
 
 while pm:
-    if cnt != K-1:
-        pm.rotate(-1)
-        cnt += 1
-    else:
-        josephus.append(pm.popleft())
-        cnt = 0
+    idx = (idx + K - 1) % len(pm)
+    josephus.append(str(pm.pop(idx)))
 
-print(f'<{", ".join(map(str, josephus))}>')
+print(f"<{', '.join(josephus)}>")
