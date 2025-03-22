5ace7505cbf5f167856e1336cfeafffef0e78c4c
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-22 20:01:57 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 100 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,29 +1,25 @@
+import sys
+
+s = ['[', ']', '(', ')']
+
 while True:
-    temp = []
-    m = input()
+    m = sys.stdin.readline().rstrip()
     if m == '.':
         break
     
     for i in m:
-        if i == '[':
-            temp.append(i)
-        elif i == ']':
-            if not temp or temp[-1] != '[':
-                temp.append(i)
-                break
-            else:
-                temp.pop()
-            
-        elif i == '(':
-                temp.append(i)
-        elif i == ')':
-            if not temp or temp[-1] != '(':
-                temp.append(i)
-                break
-            else:
-                temp.pop()
-        
-    if not temp:
+        if i not in s:
+            m = m.replace(i, '')
+    
+    while m:
+        if '()' in m:
+            m = m.replace('()', '')
+        elif '[]' in m:
+            m = m.replace('[]', '')
+        else:
+            break
+    
+    if m == '':
         print('yes')
     else:
         print('no')
