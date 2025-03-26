a8d3308e8753f421c6fbb9bd4725b27912e2f438
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-26 15:49:55 +0900

    [Silver IV] Title: solved.ac, Time: 132 ms, Memory: 37164 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -4,18 +4,12 @@ input = sys.stdin.readline
 
 n = int(input())
 
-def custom_round(n):
-    if n - int(n) >= 0.5:
-        return int(n) + 1
-    else:
-        return int(n)
-
 if n == 0:
     print(0)
 else:
     difficulty = [int(input()) for _ in range(n)]
     difficulty.sort()
-    cut = custom_round(len(difficulty) * 0.15)
+    cut = int(len(difficulty) * 0.15 + 0.5)
     difficulty_cut = difficulty[cut:len(difficulty) - cut]
-    result = custom_round(sum(difficulty_cut) / len(difficulty_cut))
+    result = int((sum(difficulty_cut) / len(difficulty_cut) + 0.5))
     print(result)
