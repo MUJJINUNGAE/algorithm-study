f7e45cfe334e1f38272939f9fa5203cacb9b7d1b
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-27 20:41:20 +0900

    [Silver III] Title: 소수 구하기, Time: 712 ms, Memory: 39208 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -3,10 +3,9 @@ square = int(N ** 0.5)
 prime = [True] * (N - M + 1)
 
 for i in range(2, square + 1):
-    start = max(i * i,(M + i - 1) // i * i)
+    start = max(i * i, (M + i - 1) // i * i)
     for j in range(start, N + 1, i):
-        if M <= j <= N:
-            prime[j - M] = False
+        prime[j - M] = False
  
 for i in range(N - M + 1):
     if prime[i] and i + M != 1:
