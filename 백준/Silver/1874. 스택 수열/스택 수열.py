d9371f491e4d838b8f13af4d906cb34b94b3fc76
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-30 08:40:36 +0900

    [Silver II] Title: 스택 수열, Time: 176 ms, Memory: 35020 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -3,57 +3,23 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-seq = [i for i in range(n, 0, -1)]
 stack = []
 result = []
+current = 1
 
-for i in range(n):
-    p = int(input())
+for _ in range(n):
+    i = int(input())
+    while current <= i:
+        stack.append(current)
+        result.append('+')
+        current += 1
     
-    if p in seq or p in stack:
-        if not stack:
-            if i == 0:
-                for _ in range(p):
-                    stack.append(seq.pop())
-                    result.append('+')
-                stack.pop()
-                result.append('-')
-            else:
-                while True:
-                    s = seq.pop()
-                    stack.append(s)
-                    result.append('+')
-                    
-                    if stack[-1] == p:
-                        stack.pop()
-                        result.append('-')
-                        break
-        else:
-            if stack[-1] >= p:
-                while True:
-                    if stack.pop() != p:
-                        result.append('-')
-                    else:
-                        result.append('-')
-                        break
-            
-            else:
-                while True:
-                    s = seq.pop()
-                    if s != p:
-                        stack.append(s)
-                        result.append('+')
-                    else:
-                        result.append('+')
-                        result.append('-')
-                        break
-            
+    if stack and stack[-1] == i:
+        stack.pop()
+        result.append('-')
     else:
-        result.append('NO')
+        result = ['NO']
         break
-    
-if 'NO' in result:
-    print('NO')
-else:
-    for i in result:
-        print(i)
+
+for i in result:
+    print(i)  
