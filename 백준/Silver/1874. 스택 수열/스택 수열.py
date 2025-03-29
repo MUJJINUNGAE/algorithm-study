826ac07b8048785745510d0cba6dc91c217e221c
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-03-30 08:30:47 +0900

    [Silver II] Title: 스택 수열, Time: 2696 ms, Memory: 38876 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,23 @@
+n = int(input())
+seq = [int(input()) for _ in range(n)]
+stack = []
+result = []
+current = 1
+
+for i in seq:
+    while current <= i:
+        stack.append(current)
+        result.append('+')
+        current += 1
+    
+    if stack and stack[-1] == i:
+        stack.pop()
+        result.append('-')
+    else:
+        result.append('NO')
+        
+if 'NO' in result:
+  print('NO')
+else:
+  for i in result:
+      print(i)
