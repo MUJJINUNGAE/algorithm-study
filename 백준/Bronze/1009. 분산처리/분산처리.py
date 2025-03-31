abb4823550bc2f1b57dcd6b4957b41bfe82f8d6c
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-04-01 00:35:45 +0900

    [Bronze II] Title: 분산처리, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1009. 분산처리/분산처리.py
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -1,7 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
 T = int(input())
 
 for _ in range(T):
     a, b = map(int, input().split())
-    answer = [a ** i % 10 for i in range(1, 5)][b % 4 - 1]
+    answer = [pow(a, i, 10) for i in range(1, 5)][b % 4 - 1]
     
     print(answer if answer != 0 else 10)
