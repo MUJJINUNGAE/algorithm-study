190fc16e2752031fc05f2180c7925c305c46fdfc
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-04-01 02:02:18 +0900

    [Silver II] Title: 통계학, Time: 392 ms, Memory: 58780 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,21 @@
+import sys
+from collections import Counter
+
+input = sys.stdin.readline
+
+N = int(input())
+num = sorted([int(input()) for _ in range(N)])
+s = sum(num)
+l = len(num)
+counter = Counter(num)
+m = max(counter.values())
+mode = []
+
+for i in counter.most_common():
+    if i[1] == m:
+        mode.append(i[0])
+        
+print(int(s/l + 0.5) if s >= 0 else -int(abs(s)/l + 0.5))
+print(num[int((l/2))])
+print(mode[0] if len(mode) == 1 else mode[1])
+print(max(num) - min(num))
