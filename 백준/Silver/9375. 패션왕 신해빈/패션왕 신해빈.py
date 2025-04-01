c834c976689f8a6141a11c08ae7dbd8a614833a2
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-04-02 06:08:02 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -0,0 +1,20 @@
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    cloth_list = {}
+    x = []
+    for _ in range(n):
+        cloth, category = input().split()
+        
+        if category not in cloth_list:
+            cloth_list[category] = [cloth]
+        else:
+            cloth_list[category].append(cloth)
+        
+    combi = 1
+    
+    for i in cloth_list:
+        combi *= (len(cloth_list[i]) + 1)
+        
+    print(combi-1)
