e1a32921169d9345963105313e2d86300d591807
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-04-03 02:41:50 +0900

    [Silver III] Title: 파도반 수열, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,13 +1,13 @@
 T = int(input())
 
 dp = [0, 1, 1, 1, 2, 2]
+N = [int(input()) for _ in range(T)]
+m = max(N)
 
-for _ in range(T):
-    N = int(input())
-    
-    if N > 5:
-        dp = dp + [0] * (N-5)
-        for i in range(6, N+1):
-            dp[i] = dp[i-1] + dp[i-5]
-            
-    print(dp[N]) 
+if m > 5:
+    dp = dp + [0] * (m-5)
+    for i in range(6, m+1):
+        dp[i] = dp[i-1] + dp[i-5]
+
+for i in N:
+    print(dp[i])
