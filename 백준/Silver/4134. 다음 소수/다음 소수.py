85c64903887aef7b17f93b168df58bff373fefa5
MUJJINUNGAE <131225238+MUJJINUNGAE@users.noreply.github.com>
2025-04-03 06:53:40 +0900

    [Silver IV] Title: 다음 소수, Time: 1756 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4134. 다음 소수/다음 소수.py
+++ 백준/Silver/4134. 다음 소수/다음 소수.py
@@ -1,13 +1,11 @@
 T = int(input())
 
-
 def is_prime(x):
-    if x == 0 or x == 1:
+    if x < 2:
         return False
-    for i in range(2, int(x ** 0.5)+1):
+    for i in range(2, int(x ** 0.5) + 1):
         if x % i == 0:
             return False
-            
     return True
 
 for _ in range(T):
